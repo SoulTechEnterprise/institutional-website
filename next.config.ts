4645354d582ed94db359c60fe8eb3345b0d549e7
LvDias <84280349+LvDias@users.noreply.github.com>
2025-12-28 20:06:18 -0300

    adds client-side caching and a sharp library

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,25 @@
 import type { NextConfig } from "next"
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+    output: "standalone",
+    onDemandEntries: {
+        maxInactiveAge: 25 * 1000,
+        pagesBufferLength: 2,
+    },
+    async headers() {
+        return [
+            {
+                source: "/:all*(svg|jpg|png)",
+                locale: false,
+                headers: [
+                    {
+                        key: "Cache-Control",
+                        value: "public, max-age=31536000, immutable",
+                    },
+                ],
+            },
+        ]
+    },
+}
 
 export default nextConfig
